9d3fddf28854d7169a157cb154b32a9f48717e42
semih.ozhan <semihozhan@yandex.com>
2022-07-07 21:00:25 +0300

    Add project files.

---
+++ JwtToken/Controllers/JwtTokenController.cs
@@ -0,0 +1,53 @@
+﻿using JwtToken.Model.ViewModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtToken.Controllers
+{
+   
+    public class JwtTokenController : ControllerBase
+    {
+        public JwtTokenController()
+        {
+        }
+
+        [HttpGet(Name = "GetToken")]
+        public IActionResult GetToken([FromBody]User user)
+        {
+            if(user.username=="semih" && user.password == "1234567") //test 
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("semihozhan_key_semihozhan_key");
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim("name","semih")
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(7),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return Ok(tokenHandler.WriteToken(token));
+            }
+            else
+            {
+                return BadRequest();
+            }
+            
+            
+        }
+
+        [Authorize(AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet(Name = "GetKoruma")]
+        public string GetKoruma()
+        {
+            return "Başarılı";
+        }
+    }
+}
---
+++ JwtToken/Model/ViewModel/User.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtToken.Model.ViewModel
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
